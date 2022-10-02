e8798ee313cdcbca98f11fd3f2a3c4dcaf53de92
avikapoor24 <avikapoor244@gmail.com>
2022-10-02 20:37:34 +0530

    Minor changes again
    Signed-off-by: avikapoor24 <avikapoor244@gmail.com>

---
+++ Programs for DS/Q1.cpp
@@ -0,0 +1,9 @@
+//Q.1] Aim 1: To insert an element at user-defined position array.                        Date-22/sept
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
